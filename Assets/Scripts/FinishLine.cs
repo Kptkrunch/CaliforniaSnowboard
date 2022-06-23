ebc1e9e1507f61ccaa68f41d1a83f97be8ef2042
Joshua T Edwards <Kptkrunch@users.noreply.github.com>
2022-06-23 12:39:16 -0500

    initial commit

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        void OnTriggerEnter2D(Collider2D other) {
+            Debug.Log("this is a test")
+        }
+    }
+}
