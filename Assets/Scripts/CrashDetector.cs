81a8d750e8cdbac46e1679486ba657b7cd6dfac0
Joshua T Edwards <Kptkrunch@users.noreply.github.com>
2022-06-23 14:04:02 -0500

    snowboarder working completely, game win and lose mechanics implemented

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    [SerializeField] float crashDelay;
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D other) {
+
+        if(other.tag == "Ground") {
+            Debug.Log("testing crash");
+            Invoke("ReloadScene", crashDelay);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void ReloadScene() {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
-
+    [SerializeField] float finishDelay;
     // Update is called once per frame
-    void Update()
-    {
-        void OnTriggerEnter2D(Collider2D other) {
-            Debug.Log("this is a test")
+    void OnTriggerEnter2D(Collider2D other) {
+
+        if(other.tag == "Player") {
+            Debug.Log("testing Finish Line");
+            Invoke("ReloadScene", finishDelay);
         }
     }
+
+    void ReloadScene() {
+        SceneManager.LoadScene(0);
+    }
+
 }
